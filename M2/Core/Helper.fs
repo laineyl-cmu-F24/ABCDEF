ee8bac8bab2c2ab3c66a5f5d55936f90fde02a57
Lainey Liu <laineyl@andrew.cmu.edu>
2024-11-10 21:54:30 -0800

    ultimately

--- M2/Core/Helper.fs
+++
@@ -1,7 +0,0 @@
-module M2.Core.Helper
-
-let map f result =
-    match result with
-    | Ok value -> Ok (f value)
-    | Error e -> Error e
-
--- M2/Core/Interfaces.fs
+++
@@ -1,9 +0,0 @@
-module M2.Core.Interfaces
-
-open M2.Core.Models
-
-type IWebSocketClient =
-    abstract member Connect : unit -> Async<Result<unit, DomainError>>
-    abstract member SendMessage<'T> : 'T -> Async<Result<unit, DomainError>>
-    abstract member ReceiveMessage : unit -> Async<Result<string, DomainError>>
-    abstract member Close : unit -> Async<Result<unit, DomainError>>
