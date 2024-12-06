edead80b37fa024dc8badc52e900418d2cb9d02e
Ran Fang <ranf@andrew.cmu.edu>
2024-12-05 17:12:38 -0800

    fixed log.txt path

--- ArbitrageGainer/Service/ApplicationService/Logger.fs
+++ ArbitrageGainer/Service/ApplicationService/Logger.fs
@@ -5,11 +5,13 @@ namespace Logging
     module Logger = 
         // Define a function to create a logger function
         let createLogger =
-            let filePath = "log.txt"
+            let filePath = Path.Combine(__SOURCE_DIRECTORY__, "log.txt")
+
             // Logger function
             let logMessage (invokingWorkflowName: string) =
                 // Prepare log entry with current date, time, and invoking workflow name
                 let logEntry = sprintf "%s - %s called" (DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff")) invokingWorkflowName
+                printfn "%A" logEntry
                 // Append log entry to the file
                 File.AppendAllText(filePath, logEntry + "\n")
             
